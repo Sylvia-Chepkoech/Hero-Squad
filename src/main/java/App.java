26e62a4a7b74e9fa39f155d044687dad32a67085
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-02 23:13:38 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation();
+    }
+}
