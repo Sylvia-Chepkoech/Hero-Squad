145ecf103a5d567bda7abfcd5f2da2c1f67dc799
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-03 13:40:37 +0300

    add squad variables

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private String squadName;
+    private String squadCause;
+    private int maxSize;
+    private ArrayList<Heros> heros;
+    private  static ArrayList<Squad> squadsInstances = new ArrayList<>();
+    private int id;
+
 }
