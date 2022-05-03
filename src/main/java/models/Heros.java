3916199b207e58e12c3916caae5b21029a585d21
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-03 13:29:31 +0300

    generate constructors for heros class

--- src/main/java/models/Heros.java
+++ src/main/java/models/Heros.java
@@ -11,4 +11,12 @@ public class Heros {
     private static ArrayList <Heros> instances = new ArrayList<>();
     private boolean squadMember;
 
+    public Heros(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
 }
