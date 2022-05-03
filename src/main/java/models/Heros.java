dda70b379fd9dd6ddcfda8c6293e71afe1295566
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-03 13:24:32 +0300

    declare variables of the heros class

--- src/main/java/models/Heros.java
+++ src/main/java/models/Heros.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Heros {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static int id;
+    private static ArrayList <Heros> instances = new ArrayList<>();
+    private boolean squadMember;
+
 }
