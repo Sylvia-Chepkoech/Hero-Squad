3ce7affc63d338186526f783827cc53168908c10
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-04 01:10:38 +0300

    add resource templates(squad-form, complete)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,40 @@ import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import models.Heros;
+import models.Squad;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/heros/new",(request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/heros", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("heroName");
+            int age=Integer.parseInt(request.queryParams("heroAge"));
+            String power=request.queryParams("heroPower");
+            String weakness=request.queryParams("heroWeakness");
+            Heros heros = new Heros(name, age, power,weakness);
+            model.put("heros", heros);
+            return new ModelAndView(model, "complete.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/heros", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Heros> heros = Heros.getAllInstances();
+            model.put("heros", heros);
+            return new ModelAndView(model, "heros.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
